e3e0dbba9bece7a30df63e8a9d72c54f00367619
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-11-25 17:03:00 -0300

    mudando mail-gun adapter

--- server-spa/src/adapters/mailgun/mailgun-mail-adapter.ts
+++ server-spa/src/adapters/mailgun/mailgun-mail-adapter.ts
@@ -1,16 +1,22 @@
 import { MailAdapter, SendMailData } from "../mail-adapter";
 
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client(
-    {username: 'api', key: process.env.MAILGUN_API_KEY});
+const nodemailer = require('nodemailer')
+const nodemailMailgun = require('nodemailer-mailgun-transport')
+
+const auth = {
+    auth: {
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: 'sandbox0f91b730368841bb8d773ae2a7df8240.mailgun.org'
+    }
+}
+
+const transporter = nodemailer.createTransport(nodemailMailgun(auth))
 
 export class MailgunMailAdapter implements MailAdapter {
 
     async sendMail({subject, body, email}: SendMailData) {
     
-        await mg.messages.create("sandbox0f91b730368841bb8d773ae2a7df8240.mailgun.org", {
+        await transporter.sendMail({
             from: `Vinicius <vinicius@mailgun.org>`,
             to: `${email}`,
             subject,
