fa2393a29a287988264fe3390353d0d241c2d008
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-11-25 15:09:44 -0300

    Conectando a API Mailgun

--- server-spa/src/adapters/mailgun/mailgun-mail-adapter.ts
+++ server-spa/src/adapters/mailgun/mailgun-mail-adapter.ts
@@ -3,7 +3,8 @@ import { MailAdapter, SendMailData } from "../mail-adapter";
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere'});
+const mg = mailgun.client(
+    {username: 'api', key: process.env.MAILGUN_API_KEY ||'a62c494c5b74f5ecd604fdf63199356a-69210cfc-40549016'});
 
 export class MailgunMailAdapter implements MailAdapter {
 
