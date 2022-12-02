a209a02a787988e641ab397e8c1e01cc69405de5
vinnypinhosantos <76694892+vinnypinhosantos@users.noreply.github.com>
2022-12-02 07:44:32 -0300

    A3

---
+++ A3/src/js/modules/masks.js
@@ -0,0 +1,41 @@
+export const masks = {
+    nome (value) {
+        return value
+        .replace(/\d/g, '')
+    },
+
+    cpf (value) {
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+    },
+
+    date (value) {
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{2})(\d{1,2,3,4})/, '$1/$2')
+        .replace(/(\d{4})\d+?$/, '$1')
+    },
+
+    fone (value) {
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(\d{4})\d+?$/, '$1')
+    },
+
+    cep (value) {
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/,'$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    }
+}
+
